bd0235f2aeef62604574f3931289600232238d72
Feu Mourek <feu.mourek@netways.de>
2019-10-25 14:32:38 +0200

    Add http perfdata widget

---
+++ src/Web/Widget/PerfdataHttp.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace ipl\Web\Widget;
+
+use ipl\Html\BaseHtmlElement;
+use ipl\Html\HtmlElement;
+
+class PerfdataHttp extends BaseHtmlElement
+{
+    protected $tag = 'div';
+
+    protected $defaultAttributes = ['id' => 'check-perfdata-http'];
+
+    protected $size;
+
+    protected $graph;
+
+    public function __construct($perfdata)
+    {
+        $values = [];
+        $labels = [];
+        foreach ($perfdata as $key => $dataset) {
+            if ($dataset->getLabel() === 'size') {
+                $this->size = $dataset->toArray()['value'];
+            } elseif (substr($dataset->getLabel(), 0, 5) === 'time_') {
+                $values[] = round($dataset->toArray()['value'], 2);
+                $labels[] = substr($dataset->getLabel(), 5);
+            } else {
+                $values[] = round($dataset->toArray()['value'], 2);
+                $labels[] = $dataset->getLabel();
+            }
+        }
+
+        $this->graph = (new VerticalBarGraph('times in ms', $values))->setLegend($labels);
+    }
+
+    public function draw()
+    {
+        $content = [
+            new HtmlElement('p', null, 'size: ' . $this->size),
+            $this->graph->draw()
+        ];
+
+        $this->setContent($content);
+
+        return $this;
+    }
+}
