cc0a61d0ce1b09c865502d810bd0aa8a1a7bf5ef
Feu Mourek <feu.mourek@netways.de>
2019-11-04 18:04:05 +0100

    Add Widget for load checks

---
+++ src/Web/Widget/PerfdataLoad.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace fpl\Web\Widget;
+
+use ipl\Html\BaseHtmlElement;
+
+class PerfdataLoad extends BaseHtmlElement
+{
+    protected $tag = 'div';
+
+    protected $defaultAttributes = ['id' => 'check-perfdata-load'];
+
+    protected $perfdata;
+
+    public function __construct($perfdata)
+    {
+        $this->perfdata = $perfdata;
+    }
+
+    public function draw()
+    {
+        $graph = new HorizontalBarGraph(null);
+        foreach ($this->perfdata as $dataset) {
+            $graph->addBar(
+                (new HorizontalBar($dataset->toArray()['label'], $dataset->toArray()['value']))
+                    ->setWarn((float) $dataset->getWarningThreshold()->getMax())
+                    ->setCrit((float) $dataset->getCriticalThreshold()->getMax())
+            );
+        }
+        $this->setContent($graph->draw());
+
+        return $this;
+    }
+}
